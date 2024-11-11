524345ec16756347551722403b90b76fe4e2eb31
Anne Medina <carmelaanne.medina@gmail.com>
2024-11-11 18:03:01 +1030

    Solution to Tenzies game using array find to see if game is won

--- App.js
+++ App.js
@@ -1,26 +1,83 @@
 import React from "react"
-import Die from "./Die.js"
-
+import Die from "./Die"
+import { nanoid } from "nanoid"
 
 export default function App() {
-    console.log("app")
-    const gameButtonValues = ["1", "2", "1", "2", "1", "2", "1", "2", "1", "2"]
 
+    const [dice, setDice] = React.useState(allNewDice())
+    const [tenzies, setTenzies] = React.useState(false)
+
+    React.useEffect(() => {
+        // console.log("Dice state changed")
+        checkIfWon() && console.log("You won!")
+    }, [dice])
+    /**
+     * Challenge: Check the dice array for these winning conditions:
+     * 1. All dice are held, and
+     * 2. all dice have the same value
+     * 
+     * If both conditions are true, set `tenzies` to true and log
+     * "You won!" to the console
+     */
+
+    function checkIfWon() {
+        let winningValue = 2 //winning value
+        let won = dice.find((die) => !(die.isHeld && die.value == winningValue)) ? false : true
+
+        // console.log(won && setTenzies(won) ? "You won!" : "Try again!");
+        return won
+    }
+
+    function generateNewDie() {
+        return {
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid()
+        }
+    }
+
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push(generateNewDie())
+        }
+        return newDice
+    }
+
+
+    function rollDice() {
+        setDice(oldDice => oldDice.map(die => {
+            return die.isHeld ?
+                die :
+                generateNewDie()
+        }))
+    }
+
+    function holdDice(id) {
+        setDice(oldDice => oldDice.map(die => {
+            return die.id === id ?
+                { ...die, isHeld: !die.isHeld } :
+                die
+        }))
+    }
+
+    const diceElements = dice.map(die => (
+        <Die
+            key={die.id}
+            value={die.value}
+            isHeld={die.isHeld}
+            holdDice={() => holdDice(die.id)}
+        />
+    ))
 
     return (
         <main>
-            <div className="game--box">
-                <h2 className="game--title">Tenzies</h2>
-                <p className="game--instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-                <div className="game--buttons-box">
-
-                    {gameButtonValues.map(buttonValue => (<Die key={Math.random(10)} value={buttonValue} />)
-                    )}
-
-                </div>
-                <button className="game--button-roll">Roll</button>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+            <div className="dice-container">
+                {diceElements}
             </div>
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
         </main>
     )
-
 }
